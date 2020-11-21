09aa148b932baa75122172820fbec348894a6d57
Slava Trenogin <slava.trenogin@jetbrains.com>
2020-11-21 14:07:53 +0300

    Yet another better httpclient registration

--- RegistrationExtensions.cs
+++ RegistrationExtensions.cs
@@ -20,32 +20,54 @@ namespace Team23.TelegramSkeleton
       where TTelegramBotClient : class, TITelegramBotClient
       where TITelegramBotClient : class, ITelegramBotClientEx
     {
-      Func<HttpClient, IServiceProvider, IEnumerable<TTelegramBotClient>> botCollectionFactory = (client, provider) =>
+      IEnumerable<T> BotCollectionFactory<T>(HttpClient client, IServiceProvider provider)
+        where T : class, ITelegramBotClient
       {
         var retriever = provider.GetService<Func<HttpClient, string, TTelegramBotClient>>();
-        if (tokensFactory(provider) is {} tokens)
+        if (tokensFactory(provider) is { } tokens)
         {
-          var result = new TTelegramBotClient[tokens.Length];
+          var result = new T[tokens.Length];
           for (var i = 0; i < tokens.Length; i++)
           {
-            result[i] = retriever(client, tokens[i]);
+            result[i] = retriever(client, tokens[i]) as T;
           }
+
           return result;
         }
 
-        return Enumerable.Empty<TTelegramBotClient>();
-      };
+        return Enumerable.Empty<T>();
+      }
 
-      TTelegramBotClient BotFactory(HttpClient client, IServiceProvider provider)
+      IDictionary<int, T> BotDictionaryFactory<T>(HttpClient client, IServiceProvider provider)
+        where T : class, ITelegramBotClient
       {
-        var registeredBots = provider.GetServices<TTelegramBotClient>();
+        var retriever = provider.GetService<Func<HttpClient, string, TTelegramBotClient>>();
+        if (tokensFactory(provider) is { } tokens)
+        {
+          var result = new Dictionary<int, T>(tokens.Length);
+          foreach (var token in tokens)
+          {
+            var bot = retriever(client, token) as T;
+            if (bot == null) continue;
+            result[bot.BotId] = bot;
+          }
+
+          return result;
+        }
+
+        return new Dictionary<int, T>(0);
+
+      }
+
+      T BotFactory<T>(HttpClient client, IServiceProvider provider)
+        where T : ITelegramBotClient
+      {
+        var registeredBots = provider.GetService<IDictionary<int, T>>();
         if (provider.GetService<IActionContextAccessor>() is {ActionContext: { } actionContext} && actionContext.RouteData.Values.TryGetValue(nameof(ITelegramBotClient.BotId), out var routeBotId))
         {
           var botId = Convert.ToInt32(routeBotId);
-          foreach (var telegramBotClient in registeredBots)
-          {
-            if (telegramBotClient.BotId == botId) return telegramBotClient;
-          }
+          if (registeredBots.TryGetValue(botId, out var bot))
+            return bot;
         }
 
         // Specific bot is unknown
@@ -54,15 +76,15 @@ namespace Team23.TelegramSkeleton
 
       services
         .AddHttpClient(nameof(TTelegramBotClient))
-        .AddTypedClient<IEnumerable<ITelegramBotClient>>(botCollectionFactory)
-        .AddTypedClient<IEnumerable<ITelegramBotClientEx>>(botCollectionFactory)
-        .AddTypedClient<IEnumerable<TITelegramBotClient>>(botCollectionFactory)
-        .AddTypedClient<IDictionary<int, ITelegramBotClient>>((client, provider) => botCollectionFactory(client, provider).ToDictionary(_ => _.BotId, _ => (ITelegramBotClient)_))
-        .AddTypedClient<IDictionary<int, ITelegramBotClientEx>>((client, provider) => botCollectionFactory(client, provider).ToDictionary(_ => _.BotId, _ => (ITelegramBotClientEx)_))
-        .AddTypedClient<IDictionary<int, TITelegramBotClient>>((client, provider) => botCollectionFactory(client, provider).ToDictionary(_ => _.BotId, _ => (TITelegramBotClient)_))
-        .AddTypedClient<ITelegramBotClient>((Func<HttpClient, IServiceProvider, TTelegramBotClient>) BotFactory)
-        .AddTypedClient<ITelegramBotClientEx>((Func<HttpClient, IServiceProvider, TTelegramBotClient>) BotFactory)
-        .AddTypedClient<TITelegramBotClient>((Func<HttpClient, IServiceProvider, TTelegramBotClient>) BotFactory);
+        .AddTypedClient(BotCollectionFactory<ITelegramBotClient>)
+        .AddTypedClient(BotCollectionFactory<ITelegramBotClientEx>)
+        .AddTypedClient(BotCollectionFactory<TITelegramBotClient>)
+        .AddTypedClient(BotDictionaryFactory<ITelegramBotClient>)
+        .AddTypedClient(BotDictionaryFactory<ITelegramBotClientEx>)
+        .AddTypedClient(BotDictionaryFactory<TITelegramBotClient>)
+        .AddTypedClient(BotFactory<ITelegramBotClient>)
+        .AddTypedClient(BotFactory<ITelegramBotClientEx>)
+        .AddTypedClient(BotFactory<TITelegramBotClient>);
     }
 
     public static void RegisterTelegramSkeleton<TTelegramBotClient>(this ContainerBuilder builder, Assembly assembly = null)
